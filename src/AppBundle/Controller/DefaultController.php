d6dad642179be39a774a786b689e6b7169a887c1
Piero Recchia <piero.recchia@gmail.com>
2015-10-21 18:40:59 -0430

    Catched exception in controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,9 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Adapter\Api\Google\Book\Adapter;
+use AppBundle\Exception\BookNotFoundException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use \Google_Service_Exception;
 
 class DefaultController extends Controller
 {
@@ -25,8 +29,19 @@ class DefaultController extends Controller
      */
     public function searchAction()
     {
-        $api = new Adapter(['key' => 'AIzaSyDfR5cB9PNeD-fn6FtEs12n5CsbFXQQgDU']);
-        dump($api->findOne('3822809128'));
-        die();
+        try {
+            $api = new Adapter(['key' => '-fn6FtEs12n5CsbFXQQgDU']);
+            dump($api->findOne('3822809128'));
+            die();
+        } catch (BookNotFoundException $e) {
+            throw new NotFoundHttpException($e->getMessage());
+        } catch (Google_Service_Exception $e) {
+            $messages = [];
+            foreach ($e->getErrors() as $error) {
+                $messages[] = implode(' | ', $error);
+            }
+            $info = implode(' - ', $messages);
+            throw new HttpException('500', $info);
+        }
     }
 }
