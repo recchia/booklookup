4fbb365b8091e15b055cc49e697eb2b302466108
Piero Recchia <piero.recchia@gmail.com>
2015-10-21 18:17:31 -0430

    Implemented findOne method in google book adapter

--- src/AppBundle/Adapter/Api/Google/Book/Adapter.php
+++ src/AppBundle/Adapter/Api/Google/Book/Adapter.php
@@ -10,17 +10,28 @@ namespace AppBundle\Adapter\Api\Google\Book;
 
 use AppBundle\Adapter\AdapterInterface;
 use AppBundle\Exception\BookNotFoundException;
+use \Google_Client;
+use \Google_Service_Books;
+use \Google_Service_Books_VolumeVolumeInfo;
+use \Google_Service_Books_VolumeVolumeInfoImageLinks;
 
 
 class Adapter implements AdapterInterface
 {
+    /**
+     * @var Google_Service_Books
+     */
+    protected $api;
 
     /**
      * @param array $config
      */
     public function __construct(array $config)
     {
-        // TODO: Implement __construct() method.
+        $client = new Google_Client();
+        $client->setApplicationName('books-lookup');
+        $client->setDeveloperKey($config['key']);
+        $this->api = new Google_Service_Books($client);
     }
 
     /**
@@ -32,7 +43,18 @@ class Adapter implements AdapterInterface
      */
     public function findOne($isbn)
     {
-        // TODO: Implement findOne() method.
+        $query = 'isbn:' . $isbn;
+        $data = [];
+        $result = $this->api->volumes->listVolumes($query, ['langRestrict' => 'es']);
+        if($result->getTotalItems() == 0) {
+            throw new BookNotFoundException(printf("ISBN %s NOT FOUND", $isbn));
+        } else {
+            foreach ($result->getItems() as $item) {
+                $data[] = $this->getBookData($item->getVolumeInfo());
+            }
+        }
+
+        return $data;
     }
 
     /**
@@ -43,5 +65,79 @@ class Adapter implements AdapterInterface
     public function find(array $isbn)
     {
         // TODO: Implement find() method.
+    }
+
+    protected function getBookData(Google_Service_Books_VolumeVolumeInfo $volume)
+    {
+        return [
+            'title' => $volume->getTitle(),
+            'author' => $this->getAuthors($volume->getAuthors()),
+            'pages' => $volume->getPageCount(),
+            'dimensions' => $volume->getDimensions(),
+            'category' => $volume->getMainCategory(),
+            'description' => $volume->getDescription(),
+            'isbn10' => $this->getIsbn($volume->getIndustryIdentifiers()),
+            'isbn13' => $this->getIsbn($volume->getIndustryIdentifiers(), 13),
+            'image' => $this->getImage($volume->getImageLinks())
+        ];
+    }
+
+    /**
+     * Get authors
+     *
+     * @param array|string $authors
+     * @return string
+     */
+    protected function getAuthors($authors)
+    {
+        if (is_array($authors)) {
+            return implode(', ', $authors);
+        } else {
+            return $authors;
+        }
+    }
+
+    /**
+     * Get ISBN 10 or 13
+     *
+     * @param array $isbn
+     * @param int $type
+     * @return string
+     */
+    protected function getIsbn(array $isbn, $type = 10)
+    {
+        switch($type) {
+            case 10:
+                return $isbn[0]['identifier'];
+            break;
+            case 13:
+                return $isbn[1]['identifier'];
+            break;
+            default:
+                return '';
+            break;
+        }
+    }
+
+    /**
+     * Get the bigger image
+     *
+     * @param Google_Service_Books_VolumeVolumeInfoImageLinks $link
+     * @return string
+     */
+    protected function getImage(Google_Service_Books_VolumeVolumeInfoImageLinks $link)
+    {
+        if(!empty($link->getLarge())) {
+            return $link->getLarge();
+        }
+        if(!empty($link->getMedium())) {
+            return $link->getMedium();
+        }
+        if(!empty($link->getSmall())) {
+            return $link->getSmall();
+        }
+        if(!empty($link->getThumbnail())) {
+            return $link->getThumbnail();
+        }
     }
 }
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Adapter\Api\Google\Book\Adapter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,5 +18,15 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+    }
+
+    /**
+     * @Route("/search")
+     */
+    public function searchAction()
+    {
+        $api = new Adapter(['key' => 'AIzaSyDfR5cB9PNeD-fn6FtEs12n5CsbFXQQgDU']);
+        dump($api->findOne('3822809128'));
+        die();
     }
 }
