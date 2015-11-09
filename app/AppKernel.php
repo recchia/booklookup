cdf1a9cf7f54b94e619abfc65b8a43f94ded2bf7
Piero Recchia <piero.recchia@gmail.com>
2015-11-09 16:48:50 -0430

    Added PhpOffice/Excel package and bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Liuggio\ExcelBundle\LiuggioExcelBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
