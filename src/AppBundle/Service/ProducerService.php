22ee52caf2599f8dbb5325ba609a63da1e8e6ad1
Piero Recchia <piero.recchia@gmail.com>
2016-01-19 15:10:39 -0430

    Added producer service class

--- src/AppBundle/Service/ProducerService.php
+++ src/AppBundle/Service/ProducerService.php
@@ -10,4 +10,15 @@ namespace AppBundle\Service;
 
 class ProducerService
 {
+    private $producer;
+
+    public function __construct($producer)
+    {
+        $this->producer = $producer;
+    }
+
+    public function publish($message)
+    {
+        $this->producer->publish(serialize($message));
+    }
 }
