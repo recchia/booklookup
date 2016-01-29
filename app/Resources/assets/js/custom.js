34756efd701e4c65be0f7f58fb9fc9d731319669
Piero Recchia <piero.recchia@gmail.com>
2016-01-29 14:08:00 -0430

    Added function to push file to queue

--- app/Resources/assets/js/custom.js
+++ app/Resources/assets/js/custom.js
@@ -1,3 +1,13 @@
 /**
  * Created by recchia on 21/10/15.
  */
+function pushFile(id) {
+  $.ajax({
+    url: Routing.generate('push_file', { id: id })
+  }).done(function(data) {
+    if (data.status == 'success') {
+      var msg = humane.create({ baseCls: 'humane-jackedup', addnCls: 'humane-jackedup-success', timeout: 6000});
+      msg.log(data.message);
+    }
+  });
+}
